5f5e067bd5ee173d78587f684e8864773dc136d7
root <root@Tank.>
2025-04-23 17:11:37 +0300

    Сервер взаимодействия 1C в Docker

---
+++ scripts/start.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+JAVA_HOME_PATH="/usr/lib/jvm/jdk-11.0.14.1-bellsoft-x86_64"
+
+CONFIG_FILE1="/etc/default/1C/1CE/1ce-cs_instance"
+CONFIG_FILE2="/etc/default/1C/1CE/1ce-elastic_instance"
+CONFIG_FILE3="/etc/default/1C/1CE/1ce-hc_instance"
+
+echo "JAVA_HOME=$JAVA_HOME_PATH" | sudo tee -a "$CONFIG_FILE1"
+echo "JAVA_HOME=$JAVA_HOME_PATH" | sudo tee -a "$CONFIG_FILE2"
+echo "JAVA_HOME=$JAVA_HOME_PATH" | sudo tee -a "$CONFIG_FILE3"
+
+ring cs --instance cs_instance service stop
+ring cs --instance cs_instance service start
+echo "Статус cs_instance"
+ring cs --instance cs_instance service status
+
+ring elasticsearch --instance elastic_instance service stop
+ring elasticsearch --instance elastic_instance service start
+echo "Статус elastic_instance"
+ring elasticsearch --instance elastic_instance service status
+
+ring hazelcast --instance hc_instance service stop
+ring hazelcast --instance hc_instance service start
+echo "Статус hc_instance"
+ring hazelcast --instance hc_instance service status
+
+echo "Пауза 30 сек"
+sleep 30
+
+echo "Инициализация базы"
+curl -Sf -X POST -H "Content-Type: application/json" \
+-d "{ \"url\" : \"jdbc:postgresql://$POSTGRES_URL\", \"username\" : \"$POSTGRES_USER\", \"password\" : \"$POSTGRES_PASSWORD\", \"enabled\" : true }" -u admin:admin http://localhost:8087/admin/bucket_server
+
+echo "Проверка работоспособности"
+sudo curl http://localhost:8087/rs/health
+
+tail -f /dev/null
+
+exec "$@"
